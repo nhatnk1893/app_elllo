e0a23d9cf54edb4ad96fdce6e8396fe8f1432c4e
NguyenKhanh Nhat <nk_nhat@brycen-vn.local>
2020-04-16 16:35:13 +0700

    .

--- lib/src/template/chapter/tab/view_script.dart
+++ lib/src/template/chapter/tab/view_script.dart
@@ -1,16 +1,32 @@
+import 'package:app_elllo/src/template/chapter/widget/audio_widget.dart';
+import 'package:audioplayers/audioplayers.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_html/flutter_html.dart';
 
 class ScriptView extends StatefulWidget {
+  final String script;
+  final String url;
+
+  const ScriptView({Key key, this.script, this.url}) : super(key: key);
+
   @override
   _ScriptViewState createState() => _ScriptViewState();
 }
 
 class _ScriptViewState extends State<ScriptView> {
+  AudioPlayer advancedPlayer = AudioPlayer();
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: Column(
-        children: [Expanded(child: null), Expanded(child: null)],
+        children: [
+          Expanded(
+              child: SingleChildScrollView(
+            child: Html(data: """${widget.script}"""),
+          )),
+          Expanded(child: ExampleApp())
+        ],
       ),
     );
   }
